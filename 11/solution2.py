7ea31a5b10dc8827919eb95efbe0ee3f0c9f7415
Linh Nguyen <nguyenlinhlinh@hotmail.com>
2024-12-11 21:04:13 +0100

    add day 1 solution 2

---
+++ 11/solution2.py
@@ -0,0 +1,42 @@
+f= open("11/input.txt", "r")
+
+input = f.readline().strip().split(" ")
+stones = {}
+for i in input:
+    stones[i] = 1
+
+for blink in range(74, -1, -1):
+    total = 0
+    next = {}
+    for  stone, count in stones.items():
+        nbr = int(stone)
+        left, right = None, None
+
+        if stone == '0':
+            left = '1'
+        elif len(stone) % 2 == 0:
+            mid = int(len(stone) / 2)
+            left = str(int(stone[: mid]))
+            right = str(int(stone[mid:]))
+        else:
+            newNbr = nbr * 2024
+            left = str(newNbr)
+    
+        if left in next:
+            next[left] += count
+        else: 
+            next[left] = count
+        if right:
+            if right in next:
+                next[right] += count
+            else:
+                next[right] = count
+    stones = next
+
+
+total = 0
+for k,v in stones.items():
+    total += v
+print(total)
+        
+# solution 242090118578155
